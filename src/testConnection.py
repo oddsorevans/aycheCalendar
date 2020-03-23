c4fe8a3cc1f4936e7926c4cf2d7d60d1ac723186
Evan Jackson <evan@Evans-MacBook-Air.local>
2020-03-23 15:03:13 -0500

    testing connection

---
+++ src/testConnection.py
@@ -0,0 +1,8 @@
+from mysql.connector import (connection)
+
+cnx = connection.MySQLConnection(
+    user = "root",
+    password = "AycheIsCool12!",
+    host = "159.89.132.49",
+    database = "phpmyadmin")
+
