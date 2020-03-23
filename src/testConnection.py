15d1cfd1b53ef41aea492cfe276b90594785ce99
Evan Jackson <evan@Evans-MacBook-Air.local>
2020-03-23 17:44:53 -0500

    testing

--- src/testConnection.py
+++ src/testConnection.py
@@ -1,8 +1,22 @@
-from mysql.connector import (connection)
+import mysql.connector
 
-cnx = connection.MySQLConnection(
-    user = "root",
-    password = "AycheIsCool12!",
-    host = "159.89.132.49",
-    database = "phpmyadmin")
+print("import finished")
 
+
+cnx = mysql.connector.connect(
+    user = 'root',
+    password = 'AycheIsCool12!',
+    host = '159.89.132.49',
+    database = 'phpmyadmin')
+cursor = cnx.cursor()
+
+print("Connection made!")
+
+query = 'SELECT COUNT(*) FROM users u WHERE u.stUserName = "streams" AND u.stPassword = "mayonnaise"'
+
+cursor.execute(query)
+
+print(cursor)
+
+cursor.close()
+cnx.close()
