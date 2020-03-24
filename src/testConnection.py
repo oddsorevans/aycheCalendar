8fd3b929e1162b7e72d67f92987f5f56396e6beb
Evan Jackson <evan@Evans-Air.attlocal.net>
2020-03-24 01:04:25 -0500

    added tunnel, need working

--- src/testConnection.py
+++ src/testConnection.py
@@ -1,22 +1,32 @@
+from sshtunnel import SSHTunnelForwarder
 import mysql.connector
 
-print("import finished")
+server = SSHTunnelForwarder(
+    '159.89.132.49',
+    ssh_username='root',
+    ssh_password='AycheisCool12!',
+    remote_bind_address=('127.0.0.1', 8080)
+)
+server.start()
 
+print("ssh complete!\nStarting connection to db")
 
 cnx = mysql.connector.connect(
     user = 'root',
-    password = 'AycheIsCool12!',
-    host = '159.89.132.49',
-    database = 'phpmyadmin')
-cursor = cnx.cursor()
+    passwd = 'AycheIsCool12!',
+    host = '127.0.0.1',
+    database = 'phpmyadmin'
+    )
+
+# cursor = cnx.cursor()
 
 print("Connection made!")
 
-query = 'SELECT COUNT(*) FROM users u WHERE u.stUserName = "streams" AND u.stPassword = "mayonnaise"'
+# query = 'SELECT COUNT(*) FROM users u WHERE u.stUserName = "streams" AND u.stPassword = "mayonnaise"'
 
-cursor.execute(query)
+# cursor.execute(query)
 
-print(cursor)
+# print(cursor)
 
-cursor.close()
-cnx.close()
+# cursor.close()
+# cnx.close()
