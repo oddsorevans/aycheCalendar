e080610ba4789b555efb6463a7d79fd6fda35be1
Daycee Duncan <dayceeduncan@gmail.com>
2020-03-25 15:59:08 -0500

    Created Splash Screen For Calendar

---
+++ src/CalendarWindow.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QDesktopWidget, QLabel
+from PyQt5.QtGui import QIcon, QPixmap, QFont
+from PyQt5.QtCore import Qt, pyqtSlot
+
+#from CalendarWindow import CalendarWindow
+
+#def sumbit_click(self):
+#        self.w = CalendarWindow()
+#        self.w.show()
+
+class CalendarWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'Ayche Calendar'
+        self.left = 10
+        self.top = 10
+        self.width = 960
+        self.height = 540
+        self.initUI()
+        
+        self.textFont = QFont()
+        self.textFont.setPointSize(72)
+        
+        # Create Label
+        self.welcomeText = QLabel(self)
+        self.welcomeText.move(240, 0)
+        self.welcomeText.resize(480, 540)
+        self.welcomeText.setFont(self.textFont)
+        self.welcomeText.setText("Welcome")
+        
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.center()
+    
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
