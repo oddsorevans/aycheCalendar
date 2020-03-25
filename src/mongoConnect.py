2b66597765a8ed7e1fa833cb5dc6365dc3ce0aab
Evan Jackson <evan@Evans-Air.attlocal.net>
2020-03-25 15:03:00 -0500

    checkLogin functional

--- src/mongoConnect.py
+++ src/mongoConnect.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import datetime
+import pprint
 
 def addUser(fn, ln, email, bd, usern, passwd):
     newUser = {
@@ -15,3 +16,15 @@ def addUser(fn, ln, email, bd, usern, passwd):
     records = db.users
     records.insert_one(newUser)
 
+def checkLogin(usern, passwd):
+    client = MongoClient("mongodb://Ayche:3Y8yqtb23lSdbVmf@ayche-shard-00-00-7twqa.mongodb.net:27017,ayche-shard-00-01-7twqa.mongodb.net:27017,ayche-shard-00-02-7twqa.mongodb.net:27017/test?ssl=true&replicaSet=Ayche-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client.get_database('Data')
+    records = db.users
+    pprint.pprint(records.find_one({
+        '$and' : [
+            {"stUsername" : "streams"},
+            {"stPassword" : "mayonnaise"}
+        ]
+    }))
+
+checkLogin("blah", "blah")
